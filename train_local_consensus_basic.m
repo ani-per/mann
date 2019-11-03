1af22ac5f49b9ba5632f898d64eed7d36665178d
ani-per <amp7226@psu.edu>
2019-11-03 18:50:09 -0500

    Dynamic data saving

--- train_local_consensus_basic.m
+++ train_local_consensus_basic.m
@@ -1,50 +1,61 @@
 %% Generation of training data through simulation of first-order system using a local protocol
 
 tic
-close all; clear colors L_list systems;
+close all; clear colors L_list sims;
 set(0, 'DefaultFigureVisible', 'off');
 set(0, 'DefaultFigureColor', [1 1 1]);
 set(0, 'DefaultAxesGridAlpha', 0.35)
 fig_size = [50 50 600 300]; fig_count = 1; p_count = 1; font_size = 17.5;
 
-n = 4; % Number of nodes, upper limit 5 for now
-sim_count = 1; % Counter of number of individual simulations
-L_count = 1; % Counter of number of unique L matrices generated
-num_runs = 4; % Counter of number of simulations for each L matrix
+% Parameters of simulation
+num_runs = 2; % Counter of number of simulations for each L matrix
 depict = false; % Plot the results for each individual simulation
 
-% Misc variables
-colors = distinguishable_colors(n);
+for n = 2:4 % Number of nodes, lower limit 2 and upper limit 5 (for now)
+    disp("n: " + n); % Track progress in L
+    sim_count = 1; % Counter of number of individual simulations
+    L_count = 1; % Counter of number of unique L matrices generated
 
-% Generate all possible Laplacians for n nodes
-L_list = generate_L_undirected(n);
-sim_count_total = size(L_list, 3)*num_runs; % Total number of individual simulations
-systems = repmat(struct('L', [], 'L_hash', [], 'X', []), 1, sim_count_total);
+    % Misc variables
+    colors = distinguishable_colors(n);
 
-for i = 1:size(L_list, 3)
+    % Generate all possible Laplacians for n nodes
+    L_list = generate_L_undirected(n);
+    sim_count_total = size(L_list, 3)*num_runs; % Total number of individual simulations
+    sims = repmat(struct('L', [], 'L_hash', [], 'X', []), 1, sim_count_total);
 
-    % Hash generation for L
-    L = L_list(:, :, i);
-    L_hash = GetMD5(L);
-    disp("L Hash: " + L_hash); % Track progress in L
+    for i = 1:size(L_list, 3)
 
-    % Domain
-    t_range = [0:0.05:10];
-    xbounds = [0, 10];
-    
-    for i = 1:num_runs
-        systems(sim_count).L = L;
-        systems(sim_count).L_hash = L_hash;
-        
-        % Track progress in individual simulations
-        disp("Sim " + sim_count + "/" + sim_count_total)
-        
-        % Simulate
-        [T, X] = simulate(@local_protocol, L, t_range, L_hash, depict, xbounds, i, colors, fig_size, font_size);
-        systems(sim_count).X = X;
-        sim_count = inc(sim_count);
+        % Hash generation for L
+        L = L_list(:, :, i);
+        L_hash = GetMD5(L);
+        disp("L Hash: " + L_hash); % Track progress in L
+
+        % Domain
+        t_range = [0:0.05:10];
+        xbounds = [0, 10];
+
+        for i = 1:num_runs
+            sims(sim_count).L = L;
+            sims(sim_count).L_hash = L_hash;
+
+            % Track progress in individual simulations
+            disp("Sim " + sim_count + "/" + sim_count_total);
+
+            % Simulate
+            [T, X] = simulate(@local_protocol, L, t_range, L_hash, depict, xbounds, i, colors, fig_size, font_size);
+            sims(sim_count).X = X;
+            sim_count = inc(sim_count);
+        end
+        L_count = inc(L_count);
     end
-    L_count = inc(L_count);
+
+    root = fullfile(pwd, "data", "local_consensus", n + "_nodes");
+    if (~exist(root, 'dir'))
+        mkdir(root);
+    end
+    save(fullfile(root, "sims_" + n + "_nodes_" + sim_count + "_sims" + ".mat"), 'sims')
+    % Import into R? https://stackoverflow.com/q/28080579
 end
 
 toc
